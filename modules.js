68f31ebe7b68028b73cd108c5df9562a32a87569
RomarioBarahona <84294268+RomarioBarahona@users.noreply.github.com>
2023-09-30 02:41:20 -0400

    worked on pictures

--- modules.js
+++ modules.js
@@ -1,43 +1,44 @@
-const toggleBtn = document.querySelector('.toggle_btn');
-const dropDownMenu = document.querySelector('.dropdown_menu');
-const links = document.querySelector('.links');
-let isMenuOpen = false;
-
-toggleBtn.addEventListener('click', function () {
-    links.classList.toggle('open');
-    isMenuOpen = !isMenuOpen;
-
-    if (isMenuOpen) {
-        dropDownMenu.style.height = dropDownMenu.scrollHeight + 'px'; // Expand to the full content height
-    } else {
-        dropDownMenu.style.height = '0'; // Close the menu
-    }
-});
-
-// Reset menu height when window is resized
-window.addEventListener('resize', function () {
-    if (!isMenuOpen) {
-        dropDownMenu.style.height = '0';
-    } else {
-        dropDownMenu.style.height = dropDownMenu.scrollHeight + 'px';
-    }
-});
-
 $(document).ready(function () {
-    // Initialize Isotope
-    var $grid = $('.menu-container').isotope({
-        itemSelector: '.menu-item',
-        layoutMode: 'fitRows'
+    const toggleBtn = $('.toggle_btn');
+    const navbar = $('.navbar');
+
+    toggleBtn.on('click', function () {
+        navbar.toggleClass('open');
+        if (navbar.hasClass('open')) {
+            // Dropdown is open, bring content down
+            $('body').css('marginTop', navbar.height() + 'px');
+        } else {
+            // Dropdown is closed, reset content position
+            $('body').css('marginTop', '0');
+        }
     });
 
-    // Filter items on button click
-    $('#menu-flters').on('click', 'li', function () {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
+    // Toggle the dropdown when clicking on the menu icon
+    $('.dropdown_menu').on('click', function () {
+        navbar.toggleClass('open');
+        if (navbar.hasClass('open')) {
+            // Dropdown is open, bring content down
+            $('body').css('marginTop', navbar.height() + 'px');
+        } else {
+            // Dropdown is closed, reset content position
+            $('body').css('marginTop', '0');
+        }
     });
+});
 
-    // Add active class to filter button
-    $('#menu-flters').on('click', 'li', function () {
-        $(this).addClass('filter-active').siblings().removeClass('filter-active');
-    });
+// Initialize Isotope
+var $grid = $('.menu-container').isotope({
+    itemSelector: '.menu-item',
+    layoutMode: 'fitRows'
+});
+
+// Filter items on button click
+$('#menu-flters').on('click', 'li', function () {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+});
+
+// Add active class to filter button
+$('#menu-flters').on('click', 'li', function () {
+    $(this).addClass('filter-active').siblings().removeClass('filter-active');
 });
